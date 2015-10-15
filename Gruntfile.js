a7b218aa6d7171d270d9501e1ee039a8318ece6f
vjscjp09 <vj.scjp009@gmail.com>
2015-10-15 15:37:43 -0400

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
         connect: {
             server: {
                 options: {
-                port: 8000,
+                port: 3000,
                 base: '.',
                 keepalive: true
                 }
